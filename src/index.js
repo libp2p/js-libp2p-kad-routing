b837468635f58680aeffde6990fa21ece88b08dc
David Dias <daviddias.p@gmail.com>
2015-08-04 21:10:57 +0200

    finish query loop and add one test with 2 peers

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 var KBucket = require('k-bucket')
-// var log = require('ipfs-logger').group('peer-routing ipfs-kad-router')
+var async = require('async')
+var log = require('ipfs-logger').group('peer-routing ipfs-kad-router')
+var protobufs = require('protocol-buffers-stream')
+var fs = require('fs')
+var Peer = require('ipfs-peer')
+var Id = require('ipfs-peer-id')
+var multiaddr = require('multiaddr')
 
 exports = module.exports = KadRouter
 
-function KadRouter (peerSelf, swarm, kBucketSize) {
+function KadRouter (peerSelf, swarmSelf, kBucketSize) {
   var self = this
 
   if (!(self instanceof KadRouter)) {
@@ -11,6 +17,10 @@ function KadRouter (peerSelf, swarm, kBucketSize) {
   }
 
   self.kBucketSize = kBucketSize || 20 // same as go-ipfs
+  self.ncp // number of closest peers to return on kBucket search
+
+  var schema = fs.readFileSync(__dirname + '/kad.proto')
+  self.createProtoStream = protobufs(schema)
 
   self.kb = new KBucket({
     localNodeId: peerSelf.id.toBytes(),
@@ -21,8 +31,6 @@ function KadRouter (peerSelf, swarm, kBucketSize) {
     var sorted = oldContacts.sort(function (a, b) {
       return a.peer.lastSeen - b.peer.lastSeen
     })
-
-    console.log('sorted len: ', sorted.length)
 
     // add all less the last seen contact
     for (var i = 0; i < sorted.length - 1; i++) {
@@ -37,16 +45,118 @@ function KadRouter (peerSelf, swarm, kBucketSize) {
 
   // register /ipfs/dht/1.0.0 protocol handler
 
-  swarm.registerHandler('/ipfs/dht/1.0.0', function (stream) {
-    // TODO(davididas)
-    //  1. attach the proto buf
-    //  2. receive the peerId the other peer is looking forward
-    //  3. get from the DHT, who are the closest peers
-    //  4. write a protobuf back with those peers
+  swarmSelf.registerHandler('/ipfs/dht/1.0.0', function (stream) {
+    var ps = self.createProtoStream()
+
+    ps.on('query', function (msg) {
+      var closerPeers = self.kb.closest({
+        id: msg.key,
+        n: self.ncp
+      })
+
+      closerPeers = closerPeers.map(function (cp) {
+        return {
+          id: cp.peer.id.toBytes(),
+          addrs: cp.peer.multiaddrs.map(function (mh) { return mh.buffer })
+        }
+      })
+
+      ps.query({
+        key: msg.key,
+        closerPeers: closerPeers
+      })
+
+      ps.finalize()
+    })
+
+    ps.pipe(stream).pipe(ps)
   })
 
+  // Interface
+
   self.findPeers = function (key, callback) {
-    // TODO (daviddias) query and fill the priority queue, call the callback with priority queue
+    // 1. get closest peers
+    // 2. ping them for more closer peers
+    // 3. use in memory qeues with async (closer peers to ping, only add them if they are not yet on the priority queue)
+    // start with an object to return then move to priority queue
+
+    var peerList = {} // < b58Id : Peer >
+
+    var q = async.queue(queryPeer, 1)
+
+    var closerPeers = self.kb.closest({
+      id: key, // key must be an Id object exported with .toBytes()
+      n: self.ncp
+    })
+
+    if (closerPeers.length === 0) {
+      return callback(new Error('kbuckets are empty'))
+    }
+
+    closerPeers.map(function (p) { q.push(p.peer) })
+
+    function queryPeer (peerToQuery, cb) {
+      // 1. query peer
+      //  1.1 open a stream
+      //  2.2 attach protobufs
+      // 2. get closer peers
+      // 3. if we already queried the peer, skip
+      // 4. if not, add to the peerList and the queue (q.push(peer))
+
+      swarmSelf.openStream(peerToQuery, '/ipfs/dht/1.0.0', function (err, stream) {
+        if (err) {
+          log.error('Could not open a stream to:', peerToQuery.id.toB58String())
+          return cb()
+        }
+
+        // consider the peer to our finger table
+        self.addPeer(peerToQuery)
+
+        peerList[peerToQuery.id.toB58String()] = peerToQuery
+
+        var ps = self.createProtoStream()
+
+        ps.on('query', function (msg) {
+          msg.closerPeers.forEach(function (closerPeer) {
+            var peerId = Id.createFromBytes(closerPeer.id)
+            var addrs = closerPeer.addrs.map(function (addr) {
+              return multiaddr(addr) // converting from buffer to multiaddr
+            })
+
+            var cPeer = new Peer(peerId, addrs)
+
+            // make sure we haven't pinged this one already
+            if (peerList[peerId.toB58String()]) {
+              return cb()
+            }
+
+            // make sure we weren't told to query ourselfs
+            if (peerId.toB58String() === peerSelf.id.toB58String()) {
+              return cb()
+            }
+
+            // add this peer to the query list
+            q.push(cPeer)
+            cb()
+          })
+        })
+
+        ps.query({
+          key: key,
+          closerPeers: []
+        })
+
+        ps.pipe(stream).pipe(ps)
+        ps.finalize()
+      })
+    }
+
+    // drain gets called after all items on the queue were process, in another words,
+    // it only happens after all callbacks were called, (which is different from the
+    // queue having 0 items to process)
+    q.drain = function done () {
+      callback(null, peerList)
+    }
   }
 
   self.addPeer = function (peer) {
@@ -56,12 +166,3 @@ function KadRouter (peerSelf, swarm, kBucketSize) {
     })
   }
 }
-
-/*
-self.candidatesToId = function (id) {
-  return kBucket.closest({
-    id: id.toBytes(),
-    n: 3
-  })
-}
-*/
--- tests/dht-test.js
+++ tests/dht-test.js
@@ -1,10 +1,10 @@
 var Lab = require('lab')
-// var Code = require('code')
+var Code = require('code')
 var lab = exports.lab = Lab.script()
 
 var experiment = lab.experiment
 var test = lab.test
-// var expect = Code.expect
+var expect = Code.expect
 
 var multiaddr = require('multiaddr')
 var Id = require('ipfs-peer-id')
@@ -16,7 +16,7 @@ var KadRouter = require('./../src')
 experiment('PING', function () {
   test('Add 10 peers to a k=2 kBucket', function (done) {
     var peerSelf = new Peer(Id.create(), [multiaddr('/ip4/127.0.0.1/tcp/4001')])
-    var swarmSelf = new Swarm(4001)
+    var swarmSelf = new Swarm()
 
     var kr = new KadRouter(peerSelf, swarmSelf, 2)
 
@@ -34,5 +34,39 @@ experiment('PING', function () {
 })
 
 experiment('QUERY', function () {
+  test('Should return error when kbucket is empty', function (done) {
+    var peerSelf = new Peer(Id.create(), [multiaddr('/ip4/127.0.0.1/tcp/4001')])
+    var swarmSelf = new Swarm()
+
+    var kr = new KadRouter(peerSelf, swarmSelf)
+
+    kr.findPeers(Id.create().toBytes(), function (err, peerList) {
+      expect(peerList).to.equal(undefined)
+      expect(err).to.be.instanceof(Error)
+      done()
+    })
+  })
+
+  test('2 peers with each other in their routing table, should return peerList of one', function (done) {
+    var peerOne = new Peer(Id.create(), [multiaddr('/ip4/127.0.0.1/tcp/8081')])
+    var swarmOne = new Swarm()
+    swarmOne.listen(8081)
+
+    var peerTwo = new Peer(Id.create(), [multiaddr('/ip4/127.0.0.1/tcp/8082')])
+    var swarmTwo = new Swarm()
+    swarmTwo.listen(8082)
+
+    var krOne = new KadRouter(peerOne, swarmOne)
+    var krTwo = new KadRouter(peerTwo, swarmTwo)
+
+    krOne.addPeer(peerTwo)
+    krTwo.addPeer(peerOne)
+
+    krOne.findPeers(Id.create().toBytes(), function (err, peerList) {
+      expect(err).to.equal(null)
+      expect(Object.keys(peerList)[0]).to.equal(peerTwo.id.toB58String())
+      done()
+    })
+  })
 
 })
